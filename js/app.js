13f1c1de2710a7fb858e9b629cd46bfe79087702
Ryan Huber <ryan.huber@mac.com>
2014-09-05 12:34:25 -0400

    adds main.js and images

---
+++ js/app.js
@@ -0,0 +1,34 @@
+angular.module("tbdModules", ["tbdModules.controllers","tbdModules.services", "ngRoute"])
+  .config(function($routeProvider){
+    $routeProvider
+      .when("/",
+      {
+        templateUrl: "views/home.html",
+        controller: "homeController"
+      })
+      .when("/notFound",
+      {
+        templateUrl: "views/notFound.html",
+        controller: "homeController"
+      })
+      .when("/timeline",
+      {
+        templateUrl: "views/timeline.html",
+        controller: "timeController"
+      })
+      .when("/gifts",{
+        templateUrl: "views/gifts.html",
+        controller: "giftController"
+      })
+      .when("/rsvp",{
+        templateUrl: "views/rsvp.html",
+        controller: "rsvpController"
+      })
+      .otherwise({
+        redirectTo: "/notFound"
+      })
+
+  });
+
+  angular.module("tbdModules.controllers", []);
+  angular.module("tbdModules.services", []);
