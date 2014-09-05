f8262cf265b86eb0717e53ee9e096224e161d022
crotten <wildrunts@gmail.com>
2014-09-04 22:37:49 -0400

    gift log is working

--- js/controllers/min/giftController-min.js
+++ js/controllers/min/giftController-min.js
@@ -1,1 +1,1 @@
-angular.module("tbdModules.controllers").controller("giftController",function(t,o,l){t.createGift=function(t){l.addGift(t),newGift=""}});
+angular.module("tbdModules.controllers").controller("giftController",function(t,e,i){t.gifts=i.getGifts(),t.createGift=function(e){i.addGift(e),t.newGift={}},t.goToTimeline=function(){e.path("/timeline")}});
