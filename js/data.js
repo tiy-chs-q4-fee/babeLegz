44f38faf47489a6fc83d0c6810d57f181d1695cf
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-05 10:39:36 -0400

    added data array

---
+++ js/data.js
@@ -0,0 +1,328 @@
+var sixteenNineMonths =
+[
+  {
+    title: "Work out your budget",
+    completed: false,
+    note:"Determine how much you have to spend, based on your families’ contributions and your own."
+  },
+  {
+    title: "Pick your wedding party",
+    completed: false,
+    note:"As soon as you’re engaged, people will start wondering who’s in."
+  },
+  {
+    title:"Start the guest list",
+    completed: false,
+    note:"Make a head count database to use throughout your planning process, with columns for contact info, RSVPs, gifts, and any other relevant information."
+  },
+  {
+    title: "Reserve your date and venues",
+    completed: false,
+    note: "Decide whether to have separate locations for the ceremony and the reception, factoring in travel time between the two places."
+  },
+  {
+    title: "Book your officiant",
+    completed: false,
+
+    note:""
+  },
+  {
+    title: "Do some research",
+    completed: false,
+    note: "Research photographers, bands, florists, and caterers."
+  },
+  {
+    title: "Throw an engagement party",
+    completed:false,
+    note: "Remember that your invitees should be on your wedding guest list as well."
+  },
+]
+
+var eightMonths =
+[
+  {
+    title:"Hire the photographer and the videographer",
+    completed:false,
+    note:"No need to talk specifics yet, but be sure that the people you hire are open to doing the shots that you want."
+  },
+  {
+    title: "Book the entertainment",
+    completed:false,
+    note: "Attend gigs of potential acts to see how they perform in front of audiences, then reserve your favorite."
+  },
+  {
+    title:"Meet caterers",
+    completed:false,
+    note:"If your wedding venue doesn’t offer its own catering service, look for one now and hire the service this month or early next."
+  },
+  {
+    title:"Purchase a dress",
+    completed:false,
+    note:"You’ll need to schedule time for at least three fittings. Veil shopping can be postponed for another two to three months."
+  },
+  {
+    title:"Reserve a block of hotel rooms for out-of-town guests",
+    completed:false,
+    note:"Pick three hotels at different price points close to the reception venue."
+  },
+  {
+    title:"Register",
+    completed:false,
+    note:"Sign up at a minimum of three retailers."
+  },
+]
+
+var sevenSixMonths =
+[
+  {
+    title:"Select and purchase invitations",
+    completed:false,
+    note:"Hire a calligrapher, if desired. Addressing cards is time-consuming, so you need to budget accordingly."
+  },
+  {
+    title:"Start planning a honeymoon",
+    completed:false,
+    note:"Make sure that your passports are up-to-date, and schedule doctors’ appointments for any shots you may need."
+  },
+  {
+    title:"Shop for bridesmaids’ dresses",
+    completed:false,
+    note:"Allow at least six months for the dresses to be ordered and sized."
+  },
+  {
+    title:"Meet with the officiant",
+    completed:false,
+    note:"Map out the ceremony and confirm that you have all the official documents for the wedding (these vary by county and religion)."
+  },
+  {
+    title:"Send save-the-date cards",
+    completed:false,
+    note:""
+  },
+  {
+    title:"Book a florist",
+    completed:false,
+    note:"Florists can serve multiple clients on one day, which is why you can wait a little longer to engage one. Plus, at this point, you’ll be firm on what your wedding palette will be."
+  },
+  {
+    title:"Arrange transportation",
+    completed:false,
+    note:"Consider limos, minibuses, trolleys, and town cars. (But know that low-to-the-ground limos can make entries and exits dicey if you’re wearing a fitted gown.)"
+  },
+  {
+    title:"Start composing a day-of timeline",
+    completed:false,
+    note:"Draw up a schedule of the event and slot in each component (the cake-cutting, the first dance)."
+  },
+]
+
+var fiveFourMonths =
+[
+  {
+    title:"Book the rehearsal and rehearsal-dinner venues",
+    completed:false,
+    note:"Negotiate the cost and the menu. If you’re planning to host a day-after brunch for guests, book that place as well."
+  },
+  {
+    title:"Check on the wedding invitations",
+    completed:false,
+    note:"Ask the stationer for samples of the finished invitations and revise them to suit your needs."
+  },
+  {
+    title:"Select and order the cake",
+    completed:false,
+    note:"Some bakers require a long lead time. Attend several tastings before committing to any baker."
+  },
+  {
+    title:"Send your guest list to the host of your shower",
+    completed:false,
+    note:"Provided you, ahem, know about the shower."
+  },
+  {
+    title:"Purchase wedding shoes and start dress fittings",
+    completed:false,
+    note:"Bring the shoes along to your first fitting so the tailor can choose the appropriate length for your gown."
+  },
+  {
+    title:"Schedule hair and makeup artists",
+    completed:false,
+    note:"Make a few appointments with local experts to try them out. Snap a photo at each so you can compare results."
+  },
+  {
+    title:"Choose your music",
+    completed:false,
+    note:"What should be playing when the wedding party is announced? During dinner? To kick off the dancing? Keep a running list of what you want—and do not want—played."
+  },
+]
+var threeMonths =
+[
+  {
+    title:"Finalize the menu and flowers",
+    completed:false,
+    note:"You’ll want to wait until now to see what will be available, since food and flowers are affected by season."
+  },
+  {
+    title:"Order favors",
+    completed:false,
+    note:"Some safe bets: monogrammed cookies or a treat that represents your city or region. If you’re planning to have welcome baskets for out-of-town guests, plan those now too."
+  },
+  {
+    title:"Finalize the readings",
+    completed:false,
+    note:"Determine what you would like to have read at the ceremony—and whom you wish to do the readings."
+  },
+  {
+    title:"Schedule your second fitting",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Finalize the order of the ceremony and the reception",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Purchase the rings",
+    completed:false,
+    note:"This will give you time for resizing and engraving."
+  },
+  {
+    title:"Send your event schedule to the vendors",
+    completed:false,
+    note:"Giving them a first draft now allows ample time for tweaks and feedback."
+  }
+
+]
+
+var twoMonths =
+[
+  {
+    title:"Touch base again with all the vendors",
+    completed:false,
+    note:"Make sure any questions you or they had on your first draft have been answered."
+  },
+  {
+    title:"Meet with the photographer",
+    completed:false,
+    note:"Discuss specific shots, and walk through the locations to note spots that appeal to you."
+  },
+  {
+    title:"Send out the invitations",
+    completed:false,
+    note:"The rule of thumb: Mail invitations six to eight weeks before the ceremony, setting the RSVP cutoff at three weeks after the postmark date."
+  },
+  {
+    title:"Have a bachelorette party",
+    completed:false,
+    note:"Arranging a night out with your girlfriends generally falls to the maid of honor. But if she hasn’t mentioned one to you by now, feel free to ask—for scheduling purposes, of course!—if a celebration is in the works."
+  }
+]
+
+var oneMonth =
+[
+  {
+    title:"Enter RSVPs into your guest-list database",
+    completed:false,
+    note:"Phone people who have not yet responded."
+  },
+  {
+    title:"Get your marriage license",
+    completed:false,
+    note:"The process can take up to six days, but it’s good to give yourself some leeway. If you are changing your name, order several copies."
+  },
+  {
+    title:"Mail the rehearsal-dinner invitations",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Visit the dressmaker for (with luck!) your last dress fitting",
+    completed:false,
+    note:"For peace of mind, you may want to schedule a fitting the week of your wedding. You can always cancel the appointment if you try on the dress then and it fits perfectly."
+  },
+  {
+    title:"Stock the bar",
+    completed:false,
+    note:"Now that you have a firm head count you can order accordingly."
+  },
+  {
+    title:"Send out as many final payments as you can",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Confirm times for hair and makeup and all vendors",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"E-mail and print directions for drivers of transport vehicles",
+    completed:false,
+    note:"This gives the chauffeurs ample time to navigate a route."
+  },
+  {
+    title:"Assign seating",
+    completed:false,
+    note:"Draw out table shapes on a layout of the room to help plan place settings. Write the names of female guests on pink sticky notes and the names of male guests on blue sticky notes so you can move people about without resketching the entire setting."
+  },
+  {
+    title:"Purchase bridesmaids’ gifts",
+    completed:false,
+    note:"You’ll present them at the rehearsal dinner."
+  },
+  {
+    title:"Write vows",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Get your hair cut and colored",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Reconfirm arrival times with vendors",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Delegate small wedding-day tasks",
+    completed:false,
+    note:"Choose someone to bustle your dress, someone to carry your things, someone to be in charge of gifts (especially the enveloped sort), someone to hand out tips, and someone to be the point person for each vendor."
+  },
+  {
+    title:"Send a timeline to the bridal party",
+    completed:false,
+    note:"Include every member’s contact information, along with the point people you’ve asked to deal with the vendors, if problems arise."
+  },
+  {
+    title:"Pick up your dress",
+    completed:false,
+    note:"Or make arrangements for a delivery."
+  },
+  {
+    title:"Check in one last time with the photographer",
+    completed:false,
+    note:"Supply him or her with a list of moments you want captured on film."
+  },
+  {
+    title:"Set aside checks for the vendors",
+    completed:false,
+    note:"And put tips in envelopes to be handed out at the event."
+  },
+  {
+    title:"Send the final guest list to the caterer and all venues hosting your wedding-related events",
+    completed:false,
+    note:"Typically, companies close their lists 72 hours in advance."
+  },
+  {
+    title:"Assemble and distribute the welcome baskets",
+    completed:false,
+    note:"",
+  },
+  {
+    title:"Pack for your honeymoon",
+    completed:false,
+    note:"",
+  },
+]
