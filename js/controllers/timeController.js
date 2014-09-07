edbcaf7558d44d7bd7001c4b0dce3d20aa6d0028
chinenyem <chinenyemnwadiugwu@chinenyems-air.gateway.2wire.net>
2014-09-07 00:48:55 -0400

    added more fx to rsvp

--- js/controllers/timeController.js
+++ js/controllers/timeController.js
@@ -1,25 +1,15 @@
 angular.module("tbdModules.controllers")
-  .controller("timeController", function($scope, $location, timelineService, dateService){
-    
-    $scope.grabDate = timelineService.getDaysUntilWedding();
-    $scope.nineToSixteenMonths = timelineService.getNineToSixteenMonths();
-    $scope.eightMonths = timelineService.getEightMonths();
-    $scope.sixToSevenMonths = timelineService.getSixToSevenMonths();
-    $scope.fourToFiveMonths = timelineService.getFourToFiveMonths();
-    $scope.threeMonths = timelineService.getThreeMonths();
-    $scope.twoMonths = timelineService.getTwoMonths();
-    $scope.oneMonth = timelineService.getOneMonth();
-
+  .controller("timeController", function($scope, $location, dateService){
+    $scope.grabDate = dateService.getDaysUntilWedding();
+    // $scope.grabMonth = dateService.monthCount($scope.grabDate.toString());
     $scope.createGift = function(gift){
       dateService.addGift(gift);
-      dateService.getGift(gift);
       $scope.newGift = {};
     };
-
+    
     $scope.goToGifts = function () {
     $location.path("/gifts");
     };
-
     $scope.goToRSVP = function () {
     $location.path("/rsvp");
     };
