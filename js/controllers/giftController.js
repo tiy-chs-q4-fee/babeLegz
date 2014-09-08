4b7f9b3986ea53b917d414cc04623fdc6433d6e4
Ryan Huber <ryan.huber@mac.com>
2014-09-08 09:09:10 -0400

    fixes controllers after merge

--- js/controllers/giftController.js
+++ js/controllers/giftController.js
@@ -1,11 +1,15 @@
 angular.module("tbdModules.controllers")
-  .controller("giftController", function($scope, $location, giftService){
+  .controller("giftController", function($scope, $location, giftService, timelineService){
     $scope.gifts = giftService.getGifts();
     $scope.createGift = function(gift){
       giftService.addGift(gift);
       $scope.newGift = {};
     };
 
+    $scope.setWeddingDate = function(weddingDate){
++      timelineService.setWeddingDate(weddingDate)
+       $location.path("/timeline")
+     }
     
 
     $scope.goToTimeline = function () {
--- js/controllers/timeController.js
+++ js/controllers/timeController.js
@@ -1,16 +1,15 @@
 angular.module("tbdModules.controllers")
-  .controller("timeController", function($scope, $location, dateService){
-    $scope.grabDate = dateService.getDaysUntilWedding();
-    // $scope.grabMonth = dateService.monthCount($scope.grabDate.toString());
-    $scope.createGift = function(gift){
-      dateService.addGift(gift);
-      $scope.newGift = {};
-    };
+
+    .controller("timeController", function($scope, $location, timelineService, dateService){
+   $scope.grabDate = timelineService.getDaysUntilWedding();
+    $scope.nineToSixteenMonths = timelineService.getNineToSixteenMonths();
+    $scope.eightMonths = timelineService.getEightMonths();
+    $scope.sixToSevenMonths = timelineService.getSixToSevenMonths();
+    $scope.fourToFiveMonths = timelineService.getFourToFiveMonths();
+    $scope.threeMonths = timelineService.getThreeMonths();
+    $scope.twoMonths = timelineService.getTwoMonths();
+    $scope.oneMonth = timelineService.getOneMonth();
+
     
-    $scope.goToGifts = function () {
-    $location.path("/gifts");
-    };
-    $scope.goToRSVP = function () {
-    $location.path("/rsvp");
-    };
+   
   });
